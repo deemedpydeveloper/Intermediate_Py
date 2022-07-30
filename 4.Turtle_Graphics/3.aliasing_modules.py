2bb52097984cfe268b81d80e54d7beb372f978c0
tejendra <tejendrayerra2000.4@gmail.com>
2022-07-30 11:23:36 +0530

    Delete 4.Turtle_Graphics directory

--- 4.Turtle_Graphics/3.aliasing_modules.py
+++
@@ -1,4 +0,0 @@
-# Aliasing Modules.
-
-import turtle as t  # to avoid typing long name of module.
-tim = t.Turtle()
--- 4.Turtle_Graphics/4.installing_modules.py
+++
@@ -1,5 +0,0 @@
-# Installing Packages.
-
-import heroes   # if displayed error, simply right-click and click 'install package'.
-print(heroes.gen())
-print(heroes.gen())  # generates everytime a new name.
