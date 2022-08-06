14d0fef965db9072c7c5c327bb75fb1e8dbdd5da
tejendra <tejendrayerra2000.4@gmail.com>
2022-08-06 08:55:28 +0530

    Add files via upload

---
+++ 9.Turtle_Crosssing_Game/0.Steps.py
@@ -0,0 +1,22 @@
+## Move turtle forward by key press.
+# create a turtle in player class.
+# create a method to move forward in player class.
+# call the method in main.py.
+
+## create the car behaviour.
+# create CarManager class.
+# create random color cars.
+# create random chance at which cars create each time.
+# create cars from right edge of screen.
+# set the cars to move by 10 paces.
+# set the car creation ranges from -250 to +250 on y-axis.
+
+## detect the collision with the car.
+# for very car check if distance of car is less than 20 paces from centre of turtle.
+
+## detect when player has reached the other side.
+# when the player reaches the other side successfully, again player needs to created at starting position.
+# the cars speed need to be increased every time and level should be increased.
+
+## create a scoreboard.
+# display game over when turtle hits the screen.
