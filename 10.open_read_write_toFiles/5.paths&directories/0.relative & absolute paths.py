b6e281ca9a757d5c3cd20f5bc1549f25f37e7145
tejendra <tejendrayerra2000.4@gmail.com>
2022-08-08 13:44:04 +0530

    Add files via upload

--- 10.open_read_write_toFiles/5.paths&directories/0.relative & absolute paths.py
+++ 10.open_read_write_toFiles/5.paths&directories/0.relative & absolute paths.py
@@ -1,5 +1,5 @@
 ## Absolute Paths.
-# /root/folder1/file1/main.py
+# /root/folder1/file1/0.pandas_library.py
 
 ## Relative Paths.
 # When you want to access files or folders in current folder ---> ./file_name or file_name.
